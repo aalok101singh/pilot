28c31659f7ca380de140316c794f9fb632b8cdfd
aalok101singh <aalok101singh@gmail.com>
2026-03-19 08:41:04 +0530

    Update DashboardLayout.tsx

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -7,12 +7,7 @@ import Topbar from '../compo
   return (
     <div className="flex h-screen bg-background text-textPrimary">
       <Sidebar currentPath={location.pathname} onNavigate={navigate} />
-      <div className="flex flex-1 flex-col min-w-0">
-        <Topbar />
-        <main className="flex-1 overflow-y-auto px-8 py-6 bg-gradient-to-b from-background to-[#050b1a]">
-          <div className="mx-auto max-w-7xl">{children}</div>
-        </main>
-      </div>
+      <div className="flex flex-1 flex-
     </div>
   );
 };
