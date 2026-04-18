4e7d5e8d8a28bd7782aeca2adcca2f1ecf61178e
aalok101singh <aalok101singh@gmail.com>
2026-04-19 04:19:55 +0530

    Update DashboardLayout.tsx

--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,8 +1,6 @@
 import { ReactNode } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import Sidebar from '../components/layout/Sidebar';
-import Topbar from '../compo
-
+import Sidebar from '../c
 
   return (
     <div className="flex h-screen bg-background text-textPrimary">
